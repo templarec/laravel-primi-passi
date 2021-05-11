9c894b62f2cf09022d9544fdd03ebe7e7685e05b
Lorenzo <templarec@gmail.com>
2021-05-11 15:25:00 +0200

    Creata pagina con routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hello');
+});
+Route::get('/contacts', function () {
+    return view('contacts');
 });
